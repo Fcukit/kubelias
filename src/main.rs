5acb97b93757950abfcc84fa0bfda8354766773b
Viktor Vlasov <vlasv90@gmail.com>
2021-07-16 11:57:54 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt, Debug)]
+#[structopt(rename_all = "kebab-case")]
+struct Opt {
+    /// kubectl config file
+    #[structopt(short = "c", long = "config")]
+    config: String,
+
+    /// kubectl namespace
+    #[structopt(short = "n", long = "namespace")]
+    namespace: String,
+
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+
+    cmd: String
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Opt::from_args();
+    println!("args: {:#?}", args);
+    Ok(())
+}
