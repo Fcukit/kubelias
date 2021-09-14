b14044271091d49ac9f6c8a880dc6887523f29fb
Viktor Vlasov <vlasv90@gmail.com>
2021-09-14 19:03:15 +0300

    [KB-1] Refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,27 @@ mod get_pod;
 mod custom_structs;
 pub use crate::custom_structs::*;
 
-pub use crate::writer::write_current_pod;
+pub use crate::writer::{write_current_pod, read_current_pod};
 pub use crate::get_pod::process;
 
 use structopt::StructOpt;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = custom_structs::Kubelias::from_args();
-    println!("{:#?}", &args);
 
     match &args.cmd {
         custom_structs::KuberCommand::GetPod(x) => {
-            println!("GetPod !!! {:#?}", &args.cmd);
             let get_pod_struct = custom_structs::GetPod { config: x.config.to_path_buf(), namespace: x.namespace.to_string() };
             let _x: Result<(), Box<dyn std::error::Error>> = get_pod::process(get_pod_struct);
         },
-        custom_structs::KuberCommand::CurrentPod => { println!("CurrentPod !!!") },
+        custom_structs::KuberCommand::CurrentPod => {
+            read_current_pod();
+        },
         _ => {
-            println!("Error !!!");
+            println!("Unknown command type!");
             ()
         }
     }
 
     Ok(())
 }
-
